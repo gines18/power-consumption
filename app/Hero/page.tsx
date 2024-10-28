eeb54bea7ba2418dc02f36c71b7b624a175f1824
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-10-28 12:45:21 +0000

    add Google Analytics

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -29,7 +29,7 @@ import Link from 'next/link';
         </header>
       </div>
     </div>
-    <img src="https://images.unsplash.com/photo-1536147116438-62679a5e01f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="Leafs" className="w-full h-48 object-cover sm:h-screen sm:w-4/12"/>
+    <img src="mainphoto.jpg" alt="Leafs" className="w-full h-48 object-cover sm:h-screen sm:w-4/12"/>
   </div>
   </>
    )
